716c6b54cc3d13fe477c7dc6c5479c2ccdc3e6cf
Christina <christinajou125@gmail.com>
2024-11-07 13:10:00 -0500

    update

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,14 +4,14 @@ namespace app\models;
 
 class Post {
     
-    public $title;
-    public $content;
+    // public $title;
+    // public $content;
 
-    // constructor for User in creating new post 
-    public function __construct($title, $content) {
-        $this->title = $title;
-        $this->content = $content;
-    }
+    // // constructor for User in creating new post 
+    // public function __construct($title, $content) {
+    //     $this->title = $title;
+    //     $this->content = $content;
+    // }
     public function getPost($params) {
         
     // Post array or database with some randomm data 
@@ -28,10 +28,18 @@ class Post {
         ]
 
     ];
-    return $allPosts;
+
+if (!empty($params['title'])) {
+    return array_filter($allposts, function ($post) use ($params) {
+        if ($user['title'] === $params['title']) {
+            return $post;
+        };
+        return null;
+    });
 }
 
-
+return $allposts;
+}
 public function savePost() {
     
 }
--- public/index.php
+++ public/index.php
@@ -1,29 +1,29 @@
 <?php
-require "../app/models/User.php";
-require "../app/controllers/UserController.php";
+require "../app/models/Post.php";
+require "../app/controllers/PostController.php";
 
-use app\controllers\UserController;
+use app\controllers\PostController;
 
-//get URI without query string
+
 $url = strtok($_SERVER["REQUEST_URI"], '?');
 
-//split url into array
+
 $uriArray = explode("/", $url);
 
-if ($uriArray[1] === 'api' && $uriArray[2] === 'users' && $_SERVER['REQUEST_METHOD'] === 'GET') {
-    $userController = new UserController();
-    $userController->getUsers();
+if ($uriArray[1] === 'api' && $uriArray[2] === 'post' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    $postController = new PostController();
+    $postController->getPost();
 }
 
-if ($uriArray[1] === 'users' && $_SERVER['REQUEST_METHOD'] === 'GET') {
-    require './views/users.html';
+if ($uriArray[1] === 'post' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    require './views/displayPost.html';
 }
 
-if ($uriArray[1] === 'api' && $uriArray[2] === 'users' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    $userController = new UserController();
-    $userController->saveUser();
+if ($uriArray[1] === 'api' && $uriArray[2] === 'post' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    $postController = new PostController();
+    $postontroller->savePost();
 }
 
-if ($uriArray[1] === 'add-users' && $_SERVER['REQUEST_METHOD'] === 'GET') {
-    require './views/add-users.html';
+if ($uriArray[1] === 'addPost' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    require './views/addPost.html';
 }
